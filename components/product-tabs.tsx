fd784e148df226e6938a6191c2395577264742f6
a96363877 <a96363877@gmail.com>
2025-03-22 00:54:36 -0700

    jj

--- components/product-tabs.tsx
+++ components/product-tabs.tsx
@@ -6,17 +6,114 @@ import { Heart } from "lucide-react"
 import AddToCartButton from "./add-to-cart-button"
 
 // Sample product data
+
 const products = [
   {
     id: 1,
-    name: "صينية كباب اللحم مع الخضار",
-    category: "جاهز للطبخ",
-    price: 7.5,
-    image: "/كباب-300x300.png",
+    name: "صينية كفتة بالطماطم",
+    category: "لحوم",
+    price: 5.0,
+    image: "/e164d4b7dc425d1954348ba34f274c04b121b68b-300x300.jpg",
     isFavorite: false,
   },
   {
     id: 2,
+    name: "صينية كفتة بالخضار",
+    category: "لحوم",
+    price: 5.0,
+    image: "/Meat-Kabab-With-Vegetables-Trayزحىل-300x300.jpg",
+    isFavorite: false,
+  },
+  {
+    id: 3,
+    name: "دجاج مشوي بالفرن مع الخضار",
+    category: "دواجن",
+    price: 3.0,
+    image: "/roasted-chicken-tray-with-vegetables.jpg",
+    isFavorite: false,
+  },
+  {
+    id: 4,
+    name: "كفتة مدورة مع خضار مشكلة",
+    category: "لحوم",
+    price: 5.99,
+    image: "/meatball-veggie-round-tray.jpg",
+    isFavorite: false,
+  },
+  {
+    id: 5,
+    name: "كفتة بالفرن مع شرائح طماطم وبصل",
+    category: "لحوم",
+    price: 6.0,
+    image: "/oven-kofta-with-tomato-onion.jpg",
+    isFavorite: false,
+  },
+  {
+    id: 6,
+    name: "ريش غنم متبلة مع خضار مشوية",
+    category: "لحوم",
+    price: 7.0,
+    image: "/marinated-lamb-chops-with-veggies.jpg",
+    isFavorite: false,
+  },
+  {
+    id: 7,
+    name: "ريش لحم بقطع ثوم وخضار",
+    category: "لحوم",
+    price: 6.1,
+    image: "/raw-lamb-ribs-with-veggies.jpg",
+    isFavorite: false,
+  },
+  {
+    id: 8,
+    name: "تشكن لولي بوب متنوع",
+    category: "دواجن",
+    price: 4.9,
+    image: "/chicken-lollipop.jpg",
+    isFavorite: false,
+  },
+  {
+    id: 9,
+    name: "ريش غنم مشكّلة بشكل مروحة",
+    category: "لحوم",
+    price: 6.0,
+    image: "/fan-shaped-lamb-chops.jpg",
+    isFavorite: false,
+  },
+  {
+    id: 10,
+    name: "شيش كباب باذنجان وكفتة",
+    category: "لحوم",
+    price: 5.0,
+    image: "/kofta-eggplant-skewers.jpg",
+    isFavorite: false,
+  },
+  {
+    id: 11,
+    name: "كفتة مع فطر على أعواد",
+    category: "لحوم",
+    price: 4.0,
+    image: "/kofta-mushroom-skewers.jpg",
+    isFavorite: false,
+  },
+  {
+    id: 12,
+    name: "كفتة بالمكسرات على أعواد",
+    category: "لحوم",
+    price: 6.0,
+    image: "/kofta-nuts-skewers.jpg",
+    isFavorite: false,
+  },
+  {
+    id: 13,
+    name: "صينية كباب اللحم مع الخضار",
+    category: "جاهز للطبخ",
+    price: 7.5,
+    image: "/5.png",
+    isFavorite: false,
+  },
+  {
+    id: 14,
     name: "خروف انجليزي مبرد",
     category: "لحم غنم",
     price: 60.0,
@@ -24,7 +121,7 @@ const products = [
     isFavorite: false,
   },
   {
-    id: 3,
+    id: 15,
     name: "صينية ستيك ريب آي متبلة, مع خضار مقطع ",
     category: "جاهز للطبخ",
     price: 6.0,
@@ -32,14 +129,15 @@ const products = [
     isFavorite: false,
   },
   {
-    id: 4,
+    id: 16,
     name: "بوكستات, لحم غنم بوكس النخبة العالي",
     category: "لحم غنم",
-    price: 55.0,
+    price: 5.0,
     image: "/777.jpg",
     isFavorite: false,
   },
-]
+];
+
 
 export default function ProductTabs() {
   const [activeTab, setActiveTab] = useState("bestsellers")
