d9a97b8cc1a01f1a28fc1268247561580d27c3bf
a96363877 <a96363877@gmail.com>
2025-03-24 04:49:23 -0700

    wsasz

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,35 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import { CartProvider } from "./contexts/cart-context"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "المتحدة | United Meat Company",
   description: "شركة المتحدة للحوم والمنتجات الغذائية",
+  openGraph: {
+    type: "website",
+    locale: "ar_SA",
+    url: "https://unionkw.com/",
+    title: "المتحدة | United Meat Company",
+    description: "شركة المتحدة للحوم والمنتجات الغذائية",
+    siteName: "المتحدة للحوم",
+    images: [
+      {
+        url: "/og-image.png", // Path to your OG image in the public directory
+        width: 1200,
+        height: 630,
+        alt: "المتحدة للحوم والمنتجات الغذائية",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "المتحدة | United Meat Company",
+    description: "شركة المتحدة للحوم والمنتجات الغذائية",
+    images: ["/og-image.png"],
+  },
 }
 
 export default function RootLayout({
@@ -17,15 +40,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ar" dir="rtl">
-      
-      <body className={inter.className} style={{zoom:0.9}}>
+      <body className={inter.className} style={{ zoom: 0.9 }}>
         <CartProvider>{children}</CartProvider>
       </body>
     </html>
   )
 }
 
-
-
-import './globals.css'
-import { CartProvider } from "./contexts/cart-context"
